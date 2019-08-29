cdffef97a7ec15f6d2ff373fe19c36b2563a6dc5
saifudin <saifudin.zezy7@gmail.com>
2019-08-29 19:50:55 +0700

    fixed filter

--- planet.js
+++ planet.js
@@ -1,8 +1,8 @@
 var planets = [];
 
-fetch('https://swapi.co/api/planets/',{
-      method:'GET'
-  })
+fetch("https://swapi.co/api/planets/", {
+  method: "GET"
+})
   .then(function(response) {
     return response.json();
   })
@@ -14,27 +14,28 @@ fetch('https://swapi.co/api/planets/',{
 
 //filter
 function filter() {
-  var q = document.getElementById('filter').nodeValue;
+  var q = document.getElementById("filter").value;
   data = planets.filter(function(planet) {
     return (
-      planet.name.toLowerCase().includes(q)
-      || planet.rotation_period.toLowerCase().includes(q)
+      planet.name.toLowerCase().includes(q) ||
+      planet.rotation_period.toLowerCase().includes(q) ||
       // || planet.orbital_period.toLowerCase().includes(q)
-      || planet.diameter.toLowerCase().includes(q)
-      || planet.climate.toLowerCase().includes(q)
-      || planet.gravity.toLowerCase().includes(q)
-      || planet.terrain.toLowerCase().includes(q)
-      || planet.surface_water.toLowerCase().includes(q)
-      || planet.population.toLowerCase().includes(q)
+      planet.diameter.toLowerCase().includes(q) ||
+      planet.climate.toLowerCase().includes(q) ||
+      planet.gravity.toLowerCase().includes(q) ||
+      planet.terrain.toLowerCase().includes(q) ||
+      planet.surface_water.toLowerCase().includes(q) ||
+      planet.population.toLowerCase().includes(q)
     );
   });
+  console.log(q);
   f_showData(data);
 }
 
 function f_showData(planets) {
   var tr, td;
   var tbody = document.getElementById("tBody");
-  tbody.innerHTML = '';
+  tbody.innerHTML = "";
   for (let index = 0; index < planets.length; index++) {
     tr = tbody.insertRow(tbody.rows.length);
     td = tr.insertCell(tr.cells.length);
